ee4ae5ac709d2cf85e3e70cf8e0a3eab5081f121
Mick Timony <michael_timony@hms.harvard.edu>
2025-03-21 17:24:52 -0400

    update rhel9 packages list to only instlal the racadm toolset

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,14 +14,8 @@ class dsu::params {
     'srvadmin-oslog', 'srvadmin-realssd', 'srvadmin-server-cli', 'srvadmin-server-snmp', 'srvadmin-smcommon', 'srvadmin-smweb',
     'srvadmin-standardAgent', 'srvadmin-storage', 'srvadmin-storage-cli', 'srvadmin-storage-snmp', 'srvadmin-storageservices',
     'srvadmin-storageservices-cli', 'srvadmin-storageservices-snmp', 'srvadmin-storelib', 'srvadmin-storelib-sysfs', 'srvadmin-sysfsutils',
-    'srvadmin-xmlsup',]
-  $srvadmin_packages_no_java_rhel_9 = ['srvadmin-base', 'srvadmin-deng', 'srvadmin-deng-snmp', 'srvadmin-hapi',
-    'srvadmin-idrac', 'srvadmin-isvc', 'srvadmin-isvc-snmp', 'srvadmin-itunnelprovider', 'srvadmin-marvellib',
-    'srvadmin-nvme', 'srvadmin-omacore', 'srvadmin-omacs', 'srvadmin-omaksmig', 'srvadmin-omcommon', 'srvadmin-omilcore', 'srvadmin-ominst',
-    'srvadmin-oslog', 'srvadmin-realssd', 'srvadmin-server-cli', 'srvadmin-server-snmp', 'srvadmin-smcommon', 'srvadmin-smweb',
-    'srvadmin-standardAgent', 'srvadmin-storage', 'srvadmin-storage-cli', 'srvadmin-storage-snmp', 'srvadmin-storageservices',
-    'srvadmin-storageservices-cli', 'srvadmin-storageservices-snmp', 'srvadmin-storelib', 'srvadmin-storelib-sysfs', 'srvadmin-sysfsutils',
-    'srvadmin-xmlsup',]
+  'srvadmin-xmlsup',]
+  $srvadmin_packages_no_java_rhel_9 = ['srvadmin-argtable2', 'srvadmin-hapi', 'srvadmin-idracadm7']
   $srvadmin_version = 'latest'
   $srvadmin_services = ['dsm_sa_eventmgrd','dsm_sa_datamgrd','dsm_sa_snmpd','dsm_om_shrsvc','instsvcdrv']
   $srvadmin_services_enable = true
