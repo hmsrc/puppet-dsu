17070716e6ea7bb19dbb16ade94bff5813884a43
Mick Timony <michael_timony@hms.harvard.edu>
2025-03-21 17:00:12 -0400

    update formatting

--- manifests/srvadmin/install.pp
+++ manifests/srvadmin/install.pp
@@ -7,29 +7,29 @@ class dsu::srvadmin::install (
   $srvadmin_version = $::dsu::srvadmin_version,
   $srvadmin_services = $::dsu::srvadmin_services,
   $srvadmin_java = 'srvadmin-jre',
-  ) {
-    notify{"start installing srvadmin packages, services: ${srvadmin_services}":}
-    notify{"Limit selection boolean: ${srvadmin_install_limited}:${::dsu::srvadmin_install_limited}, all packages boolean: ${srvadmin_install_all}, single package list: ${$srvadmin_package}":}
-    if $srvadmin_install_all == true {
-      notify{"srvadmin_package: install single meta package":}
-      package {
-        $srvadmin_package:
-          ensure  => $srvadmin_version,
-          require => Class['::dsu::repo'],
-      }
+) {
+  notify { "start installing srvadmin packages, services: ${srvadmin_services}": }
+  notify { "Limit selection boolean: ${srvadmin_install_limited}:${::dsu::srvadmin_install_limited}, all packages boolean: ${srvadmin_install_all}, single package list: ${$srvadmin_package}": }
+  if $srvadmin_install_all == true {
+    notify { 'srvadmin_package: install single meta package': }
+    package {
+      $srvadmin_package:
+        ensure  => $srvadmin_version,
+        require => Class['::dsu::repo'],
+    }
+  }
+  else {
+    notify { 'srvadmin_packages: will install multiple packages': }
+    include ::dsu::repo
+    if $::operatingsystemmajrelease in ['7','8'] {
+      #Install select dell packages from packages Array
+      ensure_packages ($srvadmin_packages_no_java, { ensure => present })
     }
     else {
-      notify{"srvadmin_packages: will install multiple packages":}
-      include ::dsu::repo
-      if $::operatingsystemmajrelease in ['7','8'] {
-        #Install select dell packages from packages Array
-        ensure_packages ($srvadmin_packages_no_java, {ensure => present})
-      }
-      else{
-         ensure_packages ($srvadmin_packages_no_java_rhel_9, {ensure => present})
-      }
-      notify{"srvadmin_packages: installed multiple packages: ${srvadmin_packages}":}
-      # If installed this will uninstall srvadmin-jre, -tomcat, -wenbserver, -all
-      ensure_packages ($srvadmin_packages_java, {ensure => absent})
+      ensure_packages ($srvadmin_packages_no_java_rhel_9, { ensure => present })
     }
+    notify { 'srvadmin_packages: installed multiple packages': }
+    # If installed this will uninstall srvadmin-jre, -tomcat, -wenbserver, -all
+    ensure_packages ($srvadmin_packages_java, { ensure => absent })
+  }
 }
